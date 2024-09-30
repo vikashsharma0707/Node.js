f85127fcbd1b7aa536ffd18e94aef87085d446d3
vikash sharma <vikashtiwari9893@gmail.com>
2024-09-30 09:13:17 +0530

    Destructuring function

--- Task.js
+++ Task.js
@@ -4,3 +4,7 @@ console.log("sum of two number is ",math.add(2,3))
 console.log("substration of two number is ",math.sub(3,2))
 
 //Destructuring
+const {add,sub} =require("./math")
+
+ console.log("sum of two number is ",add(2,3))
+ console.log("substration of two number is ",sub(3,2))
